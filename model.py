93d2faaef4994a3f6dd175ddd9377e9c1f0e1132
sb <sbenthall@gmail.com>
2019-11-04 18:02:26 -0500

    working model structure, with testing and printed accuracy, saving model. Fixes #10

--- model.py
+++ model.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
 import load_data as ld
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 
 ### where is this coming from?
 input_shape = (1025,44,1)
@@ -8,14 +13,20 @@ input_shape = (1025,44,1)
 (train_dataset, test_dataset) = ld.shuffle(train_dataset, test_dataset)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(64, (500, 5),
-                           activation='relu',
-                           input_shape=input_shape),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dropout(.1),
-    tf.keras.layers.Dense(50, activation='relu'),
-    tf.keras.layers.Dense(num_categories, activation='softmax')
+    Conv2D(32, (3, 3),
+           activation='relu',
+           padding='same',
+           input_shape=input_shape),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3),
+           activation='relu',
+           padding='same',
+           input_shape=input_shape),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(.1),
+    Dense(num_categories, activation='softmax')
 ])
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(),
@@ -24,17 +35,25 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(),
 
 model.summary()
 
-history = model.fit(train_dataset)
+history = model.fit(train_dataset,
+                    epochs=4
+)
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.plot(history.history['sparse_categorical_accuracy'], label='accuracy')
+plt.plot(history.history['loss'], label='loss')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+plt.show()
 
-model.evaluate(test_dataset)
+## testing
+print("Testing:")
 
+test_loss, test_acc = model.evaluate(test_dataset,
+                                     verbose=2)
 
+print(test_loss, test_acc)
+
+model.save("song_model.h5")
