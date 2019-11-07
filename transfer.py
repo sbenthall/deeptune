d0b7e6cb652652fcb81f2f2b477d08aa657e5148
sb <sbenthall@gmail.com>
2019-11-07 10:53:13 -0500

    cargo cult, with some tweaks, of neural style image training tutorial. #12

--- transfer.py
+++ transfer.py
@@ -126,3 +126,78 @@ for name, output in sorted(results['content'].items()):
 
 ## style transfer tutorial used up until this section:
 ## https://www.tensorflow.org/tutorials/generative/style_transfer#run_gradient_descent
+
+style_targets = extractor(chunk1)['style']
+content_targets = extractor(chunk2)['content']
+
+chunk3 = tf.Variable(chunk2)
+
+### ???
+#def clip_0_1(image):
+#    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+style_weight=1e-2
+content_weight=1e4
+
+def style_content_loss(outputs):
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                           for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                             for name in content_outputs.keys()])
+    content_loss *= content_weight / num_content_layers
+    loss = style_loss + content_loss
+    return loss
+
+
+@tf.function()
+def train_step(image):
+    with tf.GradientTape() as tape:
+        outputs = extractor(image)
+        loss = style_content_loss(outputs)
+        
+    grad = tape.gradient(loss, image)
+    opt.apply_gradients([(grad, image)])
+    image.assign(image)
+
+train_step(chunk3)
+train_step(chunk3)
+#train_step(chunk3)
+
+np.save("transfered-chunk",
+        chunk3.numpy() * 100,
+        allow_pickle=False)
+
+#tensor_to_image(image)
+
+"""
+Next steps
+
+import time
+start = time.time()
+
+epochs = 10
+steps_per_epoch = 100
+
+step = 0
+for n in range(epochs):
+  for m in range(steps_per_epoch):
+    step += 1
+    train_step(image)
+    print(".", end='')
+  display.clear_output(wait=True)
+  display.display(tensor_to_image(image))
+  print("Train step: {}".format(step))
+  
+end = time.time()
+print("Total time: {:.1f}".format(end-start))
+"""
+
+
+### Next part of tutorial
+### https://www.tensorflow.org/tutorials/generative/style_transfer#total_variation_loss
